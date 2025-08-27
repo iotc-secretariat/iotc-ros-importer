22877c8b15f077bd18e4c61c3048b1517dfe6e13
Tony Chemit <dev@tchemit.fr>
2025-08-27 08:33:23 +0200

    Add code to generate ROS ll v3.2.1

--- R/iotc_xlsx_model.R
+++ R/iotc_xlsx_model.R
@@ -629,3 +629,8 @@ generate_ros_ll_3_2_1_model <- function() {
   model_content <- load_model(json_model_path)
   write_model(ROS_LL_v3_2_1, model_content, r_file_path)
 }
+
+#' To generate ros ll v3.2.1 model
+# target_file_path <- paste0("./", ROS_LL_v3_2_1, "_model.R")
+# model_content <- load_model(output_file_path)
+# write_model(ROS_LL_v3_2_1, model_content, target_file_path)
